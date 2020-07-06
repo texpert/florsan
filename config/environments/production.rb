2f44fc6972c7803e329e9495f345e9b2b2c090ee
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-07-06 06:08:25 +0300

    Try an empty no-compress compressor to avoid `Error: Incompatible units: 'px' and 'vw'`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :scss
+  config.assets.css_compressor = NoCompression.new
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
