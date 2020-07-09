e8ea135de1d19cd238ae313e26015db72d95ccee
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-07-10 00:24:36 +0300

    Adjust Puma config to use by default 1 thread and 1 worker and ENV['RACK_ENV']

--- config/puma.rb
+++ config/puma.rb
@@ -3,37 +3,23 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-#
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 1 }
+workers_count = ENV.fetch('WEB_CONCURRENCY') { 1 }
+threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
-if ENV["RACK_ENV"] == "production"
-  # Specifies the number of `workers` to boot in clustered mode.
-  # Workers are forked web server processes. If using threads and workers together
-  # the concurrency of the application would be max `threads` * `workers`.
-  # Workers do not work on JRuby or Windows (both of which do not support
-  # processes).
-  #
-  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-  # Use the `preload_app!` method when specifying a `workers` number.
-  # This directive tells Puma to first boot the application and load code
-  # before forking the application. This takes advantage of Copy On Write
-  # process behavior so workers use less memory.
-  #
+if workers_count > 1
   preload_app!
+  workers workers_count
 end
 
 # Allow puma to be restarted by `rails restart` command.
