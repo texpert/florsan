d54ec2d32b7f3090793a9ae5598efe891b71826d
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-10-12 01:45:41 +0300

    Do not load unused Rails componnets - action_cable, active_storage, etc.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,20 @@
 
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+
+# require "action_cable/engine"
+# require "action_mailbox/engine"
+require 'action_mailer/railtie'
+require 'action_controller/railtie'
+# require 'action_text/railtie'
+require 'action_view/railtie'
+require 'active_job/railtie'
+require 'active_model/railtie'
+require 'active_record/railtie'
+# require 'active_storage/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
