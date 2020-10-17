924424ee1d23cc1ed6111efac5932c8f051eae1b
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-10-17 22:56:14 +0300

    Add hooks for content_type adding to uploaded svg-files to be displayed in Chrome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # rubocop:disable Rails/HelperInstanceVariable
+  def adjust_aws_settings(settings)
+    settings[:aws_settings][:aws_file_upload_settings] =
+      lambda { |settings| # rubocop:disable Lint/ShadowingOuterLocalVariable
+        @suppress_content_type ||= false
+        settings[:content_type] = params['file_upload']&.content_type unless @suppress_content_type
+        settings
+      }
+  end
+
+  def content_type_only_for_svg(args)
+    @suppress_content_type = true unless args[:key].match?(/\.svgz?\z/)
+  end
+  # rubocop:enable Rails/HelperInstanceVariable
 end
