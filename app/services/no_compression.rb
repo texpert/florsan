6695c77a3bc3d6ebd42ad040499cf3080e0329af
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-10-25 14:42:22 +0200

    Remove ecerything regarding assets - Sprockets isn't gonna be used

--- app/services/no_compression.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class NoCompression
-  def compress(string)
-    # TODO: Remove this either when Sprockets compressor will work, or when removing Sprockets
-    string
-  end
-end
