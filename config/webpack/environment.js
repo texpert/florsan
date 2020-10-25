68d0beffce2e4a3a56ecb7f8d4b3eb18464ff1a9
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-10-25 15:44:25 +0200

    Add erb and coffee to webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker');
+const erb = require('./loaders/erb');
+const coffee =  require('./loaders/coffee');
 
+environment.loaders.prepend('coffee', coffee);
+environment.loaders.prepend('erb', erb);
 module.exports = environment;
