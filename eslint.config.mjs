770e87ada3fe472cfa6188bb7d3b899b8261ebf2
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2024-04-30 19:35:23 +0300

    Enable Yarn PnP

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import js from "@eslint/js";
 
 export default [
-    { ignores: ["**/public/"] },
+    { ignores: ["**/public/", ".pnp.cjs", ".pnp.loader.mjs"] },
     {
         languageOptions: {
             sourceType: "module",
